f2b673c356f4b654322474210aba9f82bd83d2ab
jfkz <jfkz@users.noreply.github.com>
2020-12-13 23:40:28 +0000

    chore: pm2 and ci updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ require('dotenv-load')();
 module.exports = {
   apps: [{
     name: process.env.APP_NAME,
-    script: 'build/main.ts',
-    watch: ['build/**/*.js']
+    script: 'src/main.ts',
+    watch: ['src/**/*.{js,ts}']
   }, {
     name: process.env.APP_NAME + '-sheduler',
-    script: 'build/tasks.js',
-    watch: ['build/**/*.js']
+    script: 'src/tasks.ts',
+    watch: ['src/**/*.{js,ts}']
   }],
 };
